4ec4674b07136758cf2dd32ea96c6010584b8f58
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2024-02-23 14:39:14 +0000

    Add functions for processing JSON response objects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./reset.css";
 import "./style.css";
 
+//  document.querySelector("body").style.backgroundColor = "red"
+
 async function getWeatherData(location) {
   try {
     const response = await fetch(
@@ -8,13 +10,64 @@ async function getWeatherData(location) {
       { mode: "cors" }
     );
     const weatherData = await response.json();
+    console.log(weatherData);
     return weatherData;
   } catch {
     // alert("Unable to fetch data check internet connection")
     throw Error("Unable to fetch data check internet connection");
-    
   }
 }
 
-getWeatherData("ahinsan")
+async function getLocationData(location) {
+  const data = await getWeatherData(location);
+  const { name, country } = data.location;
+  // console.log({name, country});
+  return { name, country };
+}
 
+async function getCurrentData(location) {
+  const data = await getWeatherData(location);
+  const {
+    temp_c: tempC,
+    temp_f: tempF,
+    feelslike_c: feelsLikeC,
+    feelslike_f: feelsLikeF,
+    humidity,
+    is_day: isDay,
+  } = data.current;
+  //   console.log({  });
+  const {text: conditionText, icon: conditionIconURL} = data.current.condition;
+  return { tempC, tempF, feelsLikeC, feelsLikeF, humidity, isDay, conditionText, conditionIconURL };
+}
+
+async function getForecastData(location) {
+  const data = await getWeatherData(location);
+  const { date } = data.forecast.forecastday[0];
+  const {
+    avghumidity,
+    avgtemp_c: avgTempC,
+    avgtemp_f: avgTempF,
+    avgvis_km: avgVisKm,
+    daily_chance_of_rain: dailyChanceOfRain,
+    daily_will_it_rain: dailyWillItRain,
+  } = data.forecast.forecastday[0].day;
+  const { text: conditionText, icon: conditionIconURL } =
+    data.forecast.forecastday[0].day.condition;
+  //   console.log();
+
+  return {
+    date,
+    avghumidity,
+    avgTempC,
+    avgTempF,
+    avgVisKm,
+    dailyChanceOfRain,
+    dailyWillItRain,
+    conditionText,
+    conditionIconURL,
+  };
+}
+
+getCurrentData("ahinsan");
+getLocationData("ahinsan");
+getForecastData("ahinsan");
