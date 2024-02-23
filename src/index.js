c6cfb9d723995f110d419b8be336fb56b282c0f4
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2024-02-23 09:29:16 +0000

    Add function for retrieving weather data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import "./reset.css";
 import "./style.css";
+
+async function getWeatherData(location) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=0ccde67d368a424faf4213636240401&q=${location}&days=3&aqi=no&alerts=no`,
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    return weatherData;
+  } catch {
+    // alert("Unable to fetch data check internet connection")
+    throw Error("Unable to fetch data check internet connection");
+    
+  }
+}
+
+getWeatherData("ahinsan")
+
